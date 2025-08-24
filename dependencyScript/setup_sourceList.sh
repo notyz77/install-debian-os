6ea86e0272c9a0606d01ce928baf002b17bfb537
notyz77 <notyp4615@gmail.com>
2025-08-24 15:33:10 +0530

    Fix sourceList file https to http

--- dependencyScript/setup_sourceList.sh
+++ dependencyScript/setup_sourceList.sh
@@ -20,34 +20,34 @@ elif [ $nrelease = "testing" ]; then
     
     mv /mnt/etc/apt/sources.list /mnt/etc/apt/sources.list.bak
     cat > /mnt/etc/apt/sources.list << EOF
-deb https://deb.debian.org/debian testing main contrib non-free non-free-firmware
-deb-src https://deb.debian.org/debian testing main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian testing main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian testing main contrib non-free non-free-firmware
 
-deb https://security.debian.org/debian-security testing-security main contrib non-free non-free-firmware
-deb-src https://security.debian.org/debian-security testing-security main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security testing-security main contrib non-free non-free-firmware
+deb-src http://security.debian.org/debian-security testing-security main contrib non-free non-free-firmware
 
-deb https://deb.debian.org/debian testing-updates main contrib non-free non-free-firmware
-deb-src https://deb.debian.org/debian testing-updates main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian testing-updates main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian testing-updates main contrib non-free non-free-firmware
 
-deb https://deb.debian.org/debian testing-backports main contrib non-free non-free-firmware
-deb-src https://deb.debian.org/debian testing-backports main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian testing-backports main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian testing-backports main contrib non-free non-free-firmware
 EOF
 
 else
     
     mv /mnt/etc/apt/sources.list /mnt/etc/apt/sources.list.bak
     cat > /mnt/etc/apt/sources.list << EOF
-deb https://deb.debian.org/debian $nrelease main contrib non-free non-free-firmware
-deb-src https://deb.debian.org/debian $nrelease main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian $nrelease main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian $nrelease main contrib non-free non-free-firmware
 
-deb https://security.debian.org/debian-security $nrelease-security main contrib non-free non-free-firmware
-deb-src https://security.debian.org/debian-security $nrelease-security main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security $nrelease-security main contrib non-free non-free-firmware
+deb-src http://security.debian.org/debian-security $nrelease-security main contrib non-free non-free-firmware
 
-deb https://deb.debian.org/debian $nrelease-updates main contrib non-free non-free-firmware
-deb-src https://deb.debian.org/debian $nrelease-updates main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian $nrelease-updates main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian $nrelease-updates main contrib non-free non-free-firmware
 
-deb https://deb.debian.org/debian $nrelease-backports main contrib non-free non-free-firmware
-deb-src https://deb.debian.org/debian $nrelease-backports main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian $nrelease-backports main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian $nrelease-backports main contrib non-free non-free-firmware
 EOF
 
 fi
