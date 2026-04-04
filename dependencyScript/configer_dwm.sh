8828309a256b5fae3bf78c91ca2dc0e66e31f5f2
notyz77 <notyp4615@gmail.com>
2026-04-05 01:14:31 +0530

    changing dwm testing branch to main branch as testing is merge into main

--- dependencyScript/configer_dwm.sh
+++ dependencyScript/configer_dwm.sh
@@ -4,6 +4,6 @@ dirm="$PWD"
 
 cp $dirm/usname.txt $dirm/cable.txt $dirm/wifi.txt /mnt/tmp/
 
-git clone https://gitlab.com/ap3209/dwm_debian_apos.git /mnt/tmp/dwm_debian_apos -b debian-13-dwm
+git clone https://gitlab.com/ap3209/dwm_debian_apos.git /mnt/tmp/dwm_debian_apos
 
 chroot /mnt /bin/bash -c "cd /tmp/dwm_debian_apos && ./install.sh"
